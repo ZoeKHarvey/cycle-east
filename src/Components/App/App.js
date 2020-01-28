c8eac53e51ab5bc854c0d6c8a3bf05fb80db9ec9
Zoe <zoekharvey@gmail.com>
2020-01-27 23:49:35 -0700

    Set up barebone app file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from '../Nav/Nav'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CYCLE EAST <span>and beyond</span></h1>
+        <Nav />
+      </header>
+    </div>
+  );
+}
+
+export default App;
