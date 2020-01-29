09825c1bf48d62f2af27a82c0f22ecbb4fc5165d
Zoe <zoekharvey@gmail.com>
2020-01-28 17:30:51 -0700

    Add home component and move dark lake picture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Nav from './Components/Nav/Nav'
 import '../src/Components/App/App.css';
-import lake from '../src/assets/dark-lake.jpeg'
+import {Home} from './Components/Home/Home'
+
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <header className="App-header">
         <h1 className="h1__title">CYCLE EAST <span>and beyond</span></h1>
         <Nav />
-        <img src={lake} />
+        <Home />
       </header>
     </div>
   );
