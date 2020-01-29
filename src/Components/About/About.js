ead30e7840171a2ab72e8720e476c46e0f4c09fc
Zoe <zoekharvey@gmail.com>
2020-01-29 00:37:08 -0700

    Add about component

---
+++ src/Components/About/About.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import './Routes.css';
+
+export const Routes = () => {
+
+    return(
+      <h1>About</h1>
+    )
+  
+}
