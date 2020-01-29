edf97be66a348cebdf601c10abdde520588dbe8e
Zoe <zoekharvey@gmail.com>
2020-01-28 17:26:57 -0700

    Add dark lake picture

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Nav from './Components/Nav/Nav'
 import '../src/Components/App/App.css';
+import lake from '../src/assets/dark-lake.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CYCLE EAST <span>and beyond</span></h1>
+        <h1 className="h1__title">CYCLE EAST <span>and beyond</span></h1>
         <Nav />
+        <img src={lake} />
       </header>
     </div>
   );
