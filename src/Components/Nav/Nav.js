6acd9904bdbe49514d85b3d4f3cdb2315d0c783a
Zoe <zoekharvey@gmail.com>
2020-01-28 17:12:42 -0700

    Reset all styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import  './Nav.css'
 
 export default class Nav extends Component {
   
   render() {
     return (
-      <div>
+      <div className="div__nav">
         <h3>Home</h3>
         <h3>Routes</h3>
         <h3>About</h3>
