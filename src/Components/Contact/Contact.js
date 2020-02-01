cbe1d7acb82da1d10bf989adb507e8719b483cb8
Zoe <zoekharvey@gmail.com>
2020-02-01 11:06:02 -0700

    Add state and inputs to contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,6 +3,13 @@ import { Link } from 'react-router-dom';
 import './Contact.css';
 
 export class Contact extends Component {
+  constructor() {
+    this.state = {
+      name = '',
+      email='',
+      content = ''
+    }
+  }
 
   render() {
     return(
@@ -10,6 +17,12 @@ export class Contact extends Component {
         <h1>Contact</h1>
         <h3>Phone: 802-343-6110</h3>
         <h3>Email: jharvey@lundvt.org</h3>
+        <div className="div__contact--form">
+          <input placeholder="Name" className="input__contact--name" />
+          <input placeholder="Email *optional" className="input__contact--email" />
+          <input placeholder="Inquiry" className="input__contact--email"/>
+          <button className="button__contact--submit">Submit</button>
+        </div>
       </div>
     )
   }
