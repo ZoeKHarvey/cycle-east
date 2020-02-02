0050591d455c6ea10bce248bb65ed4f65b34cd09
Zoe <zoekharvey@gmail.com>
2020-02-02 15:03:48 -0600

    Add preparation component

---
+++ src/Components/Prep/Prep.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+export const Prep = () => {
+
+    return(
+      <div className="div__routes">
+        <h1>Prepare</h1>
+      </div>
+    )
+  
+}
